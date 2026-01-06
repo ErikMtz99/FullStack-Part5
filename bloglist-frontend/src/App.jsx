ed7795fc34d147321bfe1bf34a956f1cb1505008
Erik Martinez <ferik.mtz@outlook.com>
2026-01-05 18:32:56 -0700

    implementing animation for login

--- bloglist-frontend/src/App.jsx
+++ bloglist-frontend/src/App.jsx
@@ -1,21 +1,65 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import Notification from './components/Notification'
 import blogService from './services/blogs'
+import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [message, setMessage]=useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+    blogService.getAll().then(blogs => setBlogs( blogs ))  
   }, [])
 
-  return (
-    <div>
+  
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    console.log('logging in with: ',username, password)
+    try {
+      const user = await loginService.login({username, password,})
+      setUser(user)
+      setUsername('')
+      setPassword('')
+      setMessage('Logged In!')
+      setTimeout(() => setMessage(null), 4000)
+
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {setErrorMessage(null)}, 3000)
+    }
+  }
+
+  const loginForm = () => {
+    return(
+      <form onSubmit={handleLogin}>
+        <div> username <input value = {username} onChange={({ target }) => setUsername(target.value)}/> </div>
+        <div> password <input value = {password} onChange={({ target }) => setPassword(target.value)}/> </div>
+        <button type="submit">login</button>
+      </form>
+    )
+  }
+
+  return ( 
+    <div> 
+
+      <Notification message={message} type="success" />
+      <Notification message={errorMessage} type="error" />
+
+      {/* Renders the login form only if user equal to null */}
+      {user === null ?
+      loginForm() :
+      <div>
+        <p>{user.name} logged-in</p>
+      </div>
+    }
       <h2>blogs</h2>
       {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
+        <Blog key={blog.id} blog={blog} /> 
       )}
     </div>
   )
---
+++ bloglist-frontend/src/components/Notification.jsx
@@ -0,0 +1,25 @@
+import { useState,useEffect} from 'react'
+
+const Notification = ({ message , type }) => {
+    const [visible, setVisible] = useState(false)
+    useEffect(() => {
+        if(message) {
+            setVisible(true)
+        } else{
+            // 400ms pass before setting visible to false, allowing animation to run. After that time, visible is false, and the return of Notification is null, hence not showing anything.
+            const timeout = setTimeout(() => {setVisible(false)}, 400)
+            // this is to handle multiple timeouts and not get confused.
+            return () => clearTimeout(timeout)
+        }
+    }, [message]) //Executes every time message changes
+
+    if (!message && !visible) return null
+
+    return (
+      <div className={`notification ${type} ${!message ? 'hide':''}`}> {/* If message is false, hide is added to class */}
+        {message}
+      </div>
+    )
+  }
+
+export default Notification
